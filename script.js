8356ac80cf51606e39f9e89b0c6494a0a0f01f15
nargiz github <naliyeva15748@ada.edu.az>
2023-12-04 14:45:42 +0400

    Commit 2

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const productInfoModal = document.getElementById('product-info-modal');
     const productInfoContent = document.getElementById('product-info-content');
 
+    let products = [];
+
     const apiUrl = 'https://dummyjson.com/products';
 
     async function fetchData() {
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
+            products = data.products;
             return data.products;
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -42,17 +45,49 @@ document.addEventListener('DOMContentLoaded', () => {
         categorySelect.innerHTML = `<option value="">All Categories</option>${optionList}`;
     }
 
-    function displayProductInfo(product) {
-        productInfoContent.innerHTML = `
-            <h2>${product.title}</h2>
-            <p>Price: $${product.price}</p>
-            <p>Discount: ${product.discountPercentage}%</p>
-            <p>Category: ${product.category}</p>
-            <p>Stock: ${product.stock}</p>
-            <!-- Add additional information as needed -->
-        `;
-        productInfoModal.style.display = 'flex';
-    }
+function displayProductInfo(product) {
+    productInfoContent.innerHTML = `
+        <div class="product-info">
+            <div class="product-details">
+                <h2>${product.title}</h2>
+                <p>Price: $${product.price}</p>
+                <p>Discount: ${product.discountPercentage}%</p>
+                <p>Category: ${product.category}</p>
+                <p>Stock: ${product.stock}</p>
+                <h3>Description:</h3>
+                <p>${product.description}</p>
+            </div>
+            <div id="gallery" class="gallery"></div>
+        </div>
+        <button id="close-modal" class="close-button">Close</button>
+    `;
+
+    const galleryContainer = document.getElementById('gallery');
+    product.images.forEach((image, index) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image;
+        imgElement.alt = `${product.title} - Image ${index + 1}`;
+        imgElement.className = 'gallery-image';
+        imgElement.addEventListener('click', () => openFullSizeImage(image));
+        galleryContainer.appendChild(imgElement);
+    });
+
+    productInfoModal.style.display = 'flex';
+
+    // Add a click event listener to the close button inside the modal
+    document.getElementById('close-modal').addEventListener('click', closeProductInfoModal);
+}
+
+
+
+// Function to open the image in full size (replace with your implementation)
+function openFullSizeImage(imageUrl) {
+    // Implement your logic to display the image in full size
+    // For simplicity, you can open the image in a new tab/window.
+    window.open(imageUrl, '_blank');
+}
+
+
 
     function filterProducts() {
         const keyword = searchInput.value.toLowerCase();
@@ -60,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const filteredProducts = products.filter(product =>
             (product.title.toLowerCase().includes(keyword) ||
-             product.category.toLowerCase().includes(keyword)) &&
+                product.category.toLowerCase().includes(keyword)) &&
             (selectedCategory === '' || product.category.toLowerCase() === selectedCategory)
         );
 
@@ -72,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     fetchData().then(products => {
+        console.log(products);
         populateCategories(products);
         displayProducts(products);
         filterProducts(); // Filter on initial load
@@ -80,6 +116,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     searchInput.addEventListener('input', filterProducts);
     categorySelect.addEventListener('change', filterProducts);
-    productInfoModal.addEventListener('click', closeProductInfoModal);
     productInfoContent.addEventListener('click', event => event.stopPropagation());
 });
